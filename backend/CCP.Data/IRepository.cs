1733fb7bf76780f44bd81b80bc74474c7fafb399
stanislav.cinkl <stanislav.cinkl@student.spsmb.cz>
2025-11-20 13:55:17 +0100

    zacal jsem interfaces

---
+++ backend/CCP.Data/IRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace CCP.Data;
+
+public interface IRepository<T>
+{
+    T GetById(Guid id);
+    T[] GetAll();
+    void Add(T entity);
+    void Update(T entity);
+    void Remove(Guid id);
+
+    public class User
+    {
+        public string Username { get; set; }
+    }
+    public class Iuser
+    {
+        public string Username { get; set; }
+    }
+}
